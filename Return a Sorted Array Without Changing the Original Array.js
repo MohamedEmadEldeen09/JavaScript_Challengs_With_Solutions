a374dbee7984736856d568fb98056409157ae61d
Mohamed Emad Eldeen Mohmed Rashad <muhamedemadeldeen00@gmail.com>
2023-02-21 22:48:29 +0200

    Update Return a Sorted Array Without Changing the Original Array.js

--- Return a Sorted Array Without Changing the Original Array.js
+++ Return a Sorted Array Without Changing the Original Array.js
@@ -7,9 +7,7 @@ should return a new array, and not mutate the globalArray variable.
 
 const globalArray = [5, 6, 3, 2, 9];
 function nonMutatingSort(arr) {
-  // Only change code below this line 
    return arr.slice(0 , arr.length).sort((a,b)=>a-b)
-  // Only change code above this line
 }
 console.log(globalArray)
 console.log(nonMutatingSort(globalArray))
